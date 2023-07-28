2b3a3f41b29e8fdf1b9b5515af1c7686de95ee0f
Mikel Maron <mikel@earthgenome.org>
2023-07-28 14:24:58 -0400

    tk fix

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 var site_config = {
-    accessToken: 'pk.eyJ1IjoiZWFydGhyaXNlIiwiYSI6ImNsa2g4dmV5ZTA3NHEzeGxocHNpcjd4NXgifQ.Dzuwi3OU_PktTlXXMR7mvQ',
+    accessToken: 'pk.eyJ1IjoiZWFydGhyaXNlIiwiYSI6ImNsa2d4eGhvaTAxb3gzcHAyem5weDY5bGgifQ.dir6j-9L6fv7Q9g_DwT51w',
     mapStyle: 'mapbox://styles/earthrise/clit4wm0101wk01pd1nm7gmro',
     img_detail_zoom: 15,
     colors: {
