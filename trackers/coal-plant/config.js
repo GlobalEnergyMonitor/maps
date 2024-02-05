4c372152ceeb399245c1c8559c5972bf5e7d5ea4
Taylor Higgins <126080744+TaylorHigginsGEM@users.noreply.github.com>
2024-02-05 16:12:14 -0600

    testing for search functionality issue config.js

--- trackers/coal-plant/config.js
+++ trackers/coal-plant/config.js
@@ -1,6 +1,6 @@
 var config = {
     /* name of the data file; use key `csv` if data file is CSV format */
-    json: 'GCPT-coal-plant-2024-02-05.json',
+    json: 'coal.json',
 
     /* define the column and associated values for color application */
     color: {
