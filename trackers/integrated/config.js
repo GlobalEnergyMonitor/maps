f8332725658bd7f5dd7dadcf15b62fe85b489553
gem-tah <taylor.higgins@globalenergymonitor.org>
2024-08-02 13:45:49 -0500

    added removelastcomma to table to remove semicolon on country

--- trackers/integrated/config.js
+++ trackers/integrated/config.js
@@ -66,7 +66,8 @@ var config = {
     tableHeaders: {
         values: ['plant-/-project-name','unit-/-phase-name', 'owner', 'parent', 'capacity-(mw)', 'status', 'subnational-unit-(state,-province)', 'country/area', 'start-year', 'retired-year', 'type'],
         labels: ['Plant/project name','Unit/phase name','Owner','Parent','Capacity (MW)','Status','Subnational unit (province/state)','Country/Area','Start year','Retired year','Type'],
-        clickColumns: 'plant-/-project-name'
+        clickColumns: 'plant-/-project-name',
+        removeLastComma: ['country/area']
     },
     detailView: {
         'plant-/-project-name': {'display': 'heading'},
